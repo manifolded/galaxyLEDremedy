36533ac4fe8ff34ad1414fdc2174b3fe489d4160
Keith Matthews <keith@keithdmatthews.com>
2023-11-08 15:30:50 -0800

    Adding test executable for twinkling lights.

---
+++ Twinkle.py
@@ -0,0 +1,21 @@
+import board
+import neopixel
+import random
+import time
+
+num_pixels = 300
+my_strip = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
+loop_iteration_period = 20 # msec
+
+# Random brightness assigned to every pixel, updated periodically
+
+def twinkleFrame(my_strip):
+    for pixel_idx in range(0, num_pixels):
+        my_strip[pixel_idx] = int((1.0,1.0,1.0)*random.randint(0, 255))
+
+
+def iteration(my_strip):
+    twinkleFrame(my_strip)
+    my_strip.show()
+    time.sleep(loop_iteration_period*1000.0)
+
