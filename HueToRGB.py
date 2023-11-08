2a6ac0cae8bc9e3727edbb500e8c014f888af078
Keith Matthews <keith@keithdmatthews.com>
2023-11-07 20:32:28 -0800

    Split RainbowAndDerivs source to HueToRGB

---
+++ HueToRGB.py
@@ -0,0 +1,24 @@
+# Computes a single curve (plateau) that describes the unit green contribution
+# for any given unit hue. Does, of course, compute other colors too, suitably 
+# massaged.
+def softPlateau(self, hue_value_dec_unit, lower, upper, shoulder_width):
+    if hue_value_dec_unit < lower - shoulder_width:
+        green = 0.0
+    elif hue_value_dec_unit < lower:  # left shoulder
+        green = (hue_value_dec_unit - (lower - shoulder_width))/shoulder_width
+    elif hue_value_dec_unit < upper:    
+        green = 1.0
+    elif hue_value_dec_unit < upper + shoulder_width:  # right shoulder
+        green = (upper + shoulder_width - hue_value_dec_unit)/shoulder_width
+    else:
+        green = 0.0
+    return green
+
+# Magic numbers extracted from the features of the following image which relates
+# the unit hue with the unit rgb's.
+# https://en.wikipedia.org/wiki/Color_gradient#/media/File:Gnuplot_HSV_gradient.png
+def rgbFromHue(self, hue_value_int_rep):
+    green = int(255*self.softPlateau(hue_value_int_rep/256, 0.1666, 0.5, 0.1666))
+    red = int(255*((self.softPlateau(hue_value_int_rep/256, 0.3333, 0.6666, 0.1666)-1.0)*-1.0))
+    blue = int(255*self.softPlateau(hue_value_int_rep/256, 0.5, 0.8333, 0.1666))
+    return (red, green, blue)
--- RainbowAndDerivs.py
+++ RainbowAndDerivs.py
@@ -1,5 +1,4 @@
-import time
-
+import HueToRGB
 class Galaxy:
 
     def __init__(self) -> None:
@@ -8,32 +7,12 @@ class Galaxy:
         self.offset_idx = 0
         self.offset_speed = 3
 
-    # Does, of course, compute other colors too, suitably massaged
-    def softPlateau(self, hue_value_dec_unit, lower, upper, shoulder_width):
-        if hue_value_dec_unit < lower - shoulder_width:
-            green = 0.0
-        elif hue_value_dec_unit < lower:  # left shoulder
-            green = (hue_value_dec_unit - (lower - shoulder_width))/shoulder_width
-        elif hue_value_dec_unit < upper:    
-            green = 1.0
-        elif hue_value_dec_unit < upper + shoulder_width:  # right shoulder
-            green = (upper + shoulder_width - hue_value_dec_unit)/shoulder_width
-        else:
-            green = 0.0
-        return green
-
-    def rgbFromHue(self, hue_value_int_rep):
-        green = int(255*self.softPlateau(hue_value_int_rep/256, 0.1666, 0.5, 0.1666))
-        red = int(255*((self.softPlateau(hue_value_int_rep/256, 0.3333, 0.6666, 0.1666)-1.0)*-1.0))
-        blue = int(255*self.softPlateau(hue_value_int_rep/256, 0.5, 0.8333, 0.1666))
-        return (red, green, blue)
-
     def rainbowFrame(self, my_strip):
         global offset_idx
         for pixel_idx in range(self.num_pixels):
             hue_idx = (pixel_idx+offset_idx)%self.num_hues 
             hue_value_int_rep = (hue_idx/self.num_hues)*255
-            my_strip[pixel_idx] = self.rgbFromHue(hue_value_int_rep)
+            my_strip[pixel_idx] = HueToRGB.rgbFromHue(hue_value_int_rep)
 
     def galaxyHue(self, hue_value_dec_unit):
         return 0.5*hue_value_dec_unit + 0.5
@@ -45,7 +24,7 @@ class Galaxy:
             hue_idx = (pixel_idx+self.offset_idx)%self.num_hues
             hue_value_dec_unit = hue_idx/self.num_hues
             galaxy_hue = self.galaxyHue(hue_value_dec_unit)
-            my_strip[pixel_idx] = self.rgbFromHue(galaxy_hue*255)
+            my_strip[pixel_idx] = HueToRGB.rgbFromHue(galaxy_hue*255)
 
     # This is the atom that is repeated endlessly to form the display
     # It is to be called from the parent code as the iterations of the loop,
