d130bd7409cbeded026e964ea72b447f2b87fca8
Keith Matthews <keith@keithdmatthews.com>
2023-11-07 17:02:37 -0800

    current_stars now treated as member data

--- StarField.py
+++ StarField.py
@@ -45,27 +45,26 @@ class Stars:
     def testForInstantiation(self, new_star):
         return time.time() >= new_star['inception_time'] and not new_star['instantiated']
 
-    def destroy_stars(self, current_stars, my_strip):
-        for star in current_stars:
+    def destroy_stars(self, my_strip):
+        for star in self.current_stars:
             if time.time() > star['destruction_time']:
                 my_strip[star['pixel_index']] = (0,0,0)
                 my_strip.show()
-                current_stars.remove(star)
+                self.current_stars.remove(star)
                 # lastly, everytime you destroy a star you create a new one.
                 # Must be done here since here we can replace them
                 replacement_star = self.define_star()
-                current_stars.append(replacement_star)
+                self.current_stars.append(replacement_star)
                 # leave this star to be initialized on the next event loop iteration
 
 
     # evolve stars
     def iteration(self, my_strip):
-        global current_stars
         # Check if new_star is ready yet and if so instantiate
-        for star in current_stars:
+        for star in self.current_stars:
             if self.testForInstantiation(star):
                 self.instantiate_star(star, my_strip)
         # Clean up any expired stars
-        self.destroy_stars(current_stars)
+        self.destroy_stars()
 
 
