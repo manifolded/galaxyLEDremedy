215cfb4a9b89c458acafc707c5187954bd9cf2b1
Keith Matthews <keith@keithdmatthews.com>
2023-11-07 16:30:42 -0800

    Dropped silly list of functions approach in favor of if statements

--- ControlPanel-guizero.py
+++ ControlPanel-guizero.py
@@ -14,25 +14,26 @@ num_pixels = 300
 my_strip = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 loop_iteration_period = 20 # msec
 
-displays = ["none", "rainbow", "galaxy", "starfield"]
 galaxy = RainbowAndDerivs.Galaxy()
 stars = StarField.Stars()
-implementations = [None, None, galaxy.iteration, stars.iteration]
+# displays = ["none", "rainbow", "galaxy", "starfield"]
+# implementations = [None, None, galaxy.iteration(), stars.iteration]
 current_display = 0
 
 # Display codes can't be blocking so they have to be some sort of thread. We have to be able 
 # to click the terminate button. One main loop isn't very appealing since the codes are so 
 # different. Separate loops which terminate on command is more appealing.
-
 def loop_iteration():
     global current_display
-    if current_display >= 2:
-        implementations[current_display]
-        # No drawing is allowed in the display code, so it must get invoked here.
-        my_strip.show()
+    if current_display == 2:
+        galaxy.iteration(my_strip)
+    elif current_display == 3:
+        stars.iteration(my_strip)
     else:
         my_strip.fill((0,0,0))
         my_strip.show()
+    # No drawing is allowed in the display code, so it must get invoked here.
+    my_strip.show()
 
 
 def execute_galaxy():
